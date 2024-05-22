35d91d2732629e9ee3de86008ca61af57874ac83
DongKey03 <dongpham24703@gmail.com>
2024-05-22 18:23:58 +0700

    feat: add IntroActivity.java

---
+++ app/src/main/java/com/example/appshopsport/IntroActivity.java
@@ -0,0 +1,38 @@
+package com.example.appshopsport;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Delay for 3 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start LoginActivity
+                Intent intent_intro = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent_intro);
+                // Close IntroActivity
+                finish();
+            }
+        }, 3000); // 3000 milliseconds delay
+    }
+}
